7e1eca655eaf5da2ced44164335dff1174fbbe53
Alejandrox <droxgamedev@gmail.com>
2025-04-23 12:34:50 +0200

    problema Normal Map para animaciones solucionado LOLOLOLOLOLOLOLO
    LOLOLOLOLOLOLOLO

--- Assets/Scripts/PlayerAnimations.cs
+++ Assets/Scripts/PlayerAnimations.cs
@@ -1,23 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerAnimations : MonoBehaviour
 {
-    
-    public string[] AnimationNames;
-    public Texture2D[] NormalMaps;
+
+    private Animator _anim;   
+    private Material _mat;
+    public string[] animationName;
+    public Texture2D[] normalMaps;
 
     // Start is called before the first frame update
     void Start()
     {
+        _anim = GetComponent<Animator>();
+        //Renderer renderer = GetComponent<Renderer>();
+        //_mat = renderer.material;
+
         
+        /*
+        _mat.EnableKeyword("_NORMALMAP");
+        _mat.SetTexture("_NormalMap", normalMaps[1]);
+        */
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _anim.CrossFade(animationName[1], 0, 0);
     }
 }
