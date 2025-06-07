91f8f30bcaa6e727b3cec85992bcc15c62a82e50
Alejandrox <droxgamedev@gmail.com>
2025-06-07 12:03:56 +0200

    Vial Sprites

--- Assets/Scripts/Vial.cs
+++ Assets/Scripts/Vial.cs
@@ -11,4 +11,48 @@ public enum VialType
 public class Vial : AffectedByGravity
 {
     public VialType type;
+
+    [Space(10)]
+
+    [Header("Metal Vial")]
+    public SpriteRenderer metalVialSpriteRenderer;
+    public Sprite metalVialSpriteIron;
+    public Sprite metalVialSpriteSteel;
+    public Sprite metalVialSpriteTin;
+    public Sprite metalVialSpritePewter;
+    
+    [Space(10)]
+
+    [Header("Metal Indicator")]
+    public SpriteRenderer metalSymbolSpriteRenderer;
+    public Sprite metalSymbolSpriteIron;
+    public Sprite metalSymbolSpriteSteel;
+    public Sprite metalSymbolSpriteTin;
+    public Sprite metalSymbolSpritePewter;
+    
+
+    void Awake() {
+    
+        switch (type) {
+            case VialType.iron:
+                metalVialSpriteRenderer.sprite = metalVialSpriteIron;
+                metalSymbolSpriteRenderer.sprite = metalSymbolSpriteIron;
+                break;
+            case VialType.steel:
+                metalVialSpriteRenderer.sprite = metalVialSpriteSteel;
+                metalSymbolSpriteRenderer.sprite = metalSymbolSpriteSteel;
+                break;
+            case VialType.tin:
+                metalVialSpriteRenderer.sprite = metalVialSpriteTin;
+                metalSymbolSpriteRenderer.sprite = metalSymbolSpriteTin;
+                break;
+            case VialType.pewter:
+                metalVialSpriteRenderer.sprite = metalVialSpritePewter;
+                metalSymbolSpriteRenderer.sprite = metalSymbolSpritePewter;
+                break;
+        }
+
+    }
 }
+
+
